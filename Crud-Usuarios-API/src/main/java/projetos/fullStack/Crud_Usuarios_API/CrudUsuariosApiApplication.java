3063449bec250fcf0446deb583093283e6b99e88
Matheusgabriel1234 <magaje2005@gmail.com>
2025-04-16 10:23:22 -0300

    Initial Commit

---
+++ Crud-Usuarios-API/src/main/java/projetos/fullStack/Crud_Usuarios_API/CrudUsuariosApiApplication.java
@@ -0,0 +1,13 @@
+package projetos.fullStack.Crud_Usuarios_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsuariosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsuariosApiApplication.class, args);
+	}
+
+}
